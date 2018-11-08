bbf7487dacd78075b15b8495405d7aea6e0c5048
aaron <aaron@asouer.com>
2018-11-07 21:03:10 -0600

    added working on api return messages

--- SpawnPageUserAPI/application.py
+++ SpawnPageUserAPI/application.py
@@ -1,9 +1,25 @@
 from flask import Flask, render_template, redirect, url_for, request
+from werkzeug.debug import DebuggedApplication
 from config import ConfigPicker
 import os
 
 conf = ConfigPicker(os.environ['ENV'])
 
+# app = Flask(__name__)
+# app.config.from_object(conf)
 
-app = Flask(__name__)
-app.config.from_object(conf)
+
+def create_app():
+    # Insert whatever else you do in your Flask app factory.
+
+    app = Flask(__name__)
+    app.config.from_object(conf)
+
+    if app.debug:
+        app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
+
+    return app
+
+
+app = create_app()
+
